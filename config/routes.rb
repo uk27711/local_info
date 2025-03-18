fd214ea939d3b58f647ff6c90f4409261935bfa7
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2025-03-18 11:27:19 +0000

    20250318

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   post 'posts' => 'posts#create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
+  get 'top', to: 'homes#top', as: 'top_homes'
+  
     resources :posts, only: [:index, :show, :edit, :create, :update, :destroy] do
   end
     resources :users, only: [:new, :create, :show, :edit, :update, :index, :destroy] do
   end 
-
 end
